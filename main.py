50b3cf8cef53eeb8664c721da8d26ca2dea0e556
hiroxkei <1465050994@qq.com>
2025-04-16 21:47:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def search_image_url(query):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0",
         "Referer": "https://s.taobao.com/"
-        "Cookie": os.getenv("TAOBAO_COOKIE")
+        "Cookie": cookie or os.getenv("TAOBAO_COOKIE")
     }
     # 淘宝搜索 URL（搜索结果页）
     search_url = f"https://s.taobao.com/search?q={query}"
